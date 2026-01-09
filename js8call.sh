31a38731c7108807d8ba6608cfb8d0c0816cc095
alexvesey <alex.vesey@none.com>
2026-01-09 18:27:56 -0500

    fix: update js8 call script with warning message about venv setup

--- js8call.sh
+++ js8call.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cd ~ 
+if [ ! -d ".venv" ]; then
+    echo "Error: .venv does not exist in the current directory. Follow 'Venv Setup Instructions' in the README."
+    exit 1
+fi
 source .venv/bin/activate
 python3 js8.py --std
